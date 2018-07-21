a8f7a5060e2c4d2ac054217b2d0d1c45e92c7c7e
Angad Singh <angadsingh567890@gmail.com>
2018-07-22 02:26:03 +0530

    Finish displaying todo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
 import React, { Component } from "react";
 
-const Header = props => {
-  return (
-    <h1
-      style={{
-        fontWeight: 200,
-        fontSize: "3rem",
-        marginBottom: 30,
-        textTransform: "uppercase"
-      }}
-    >
-      {props.title}
-    </h1>
-  );
-};
+const Header = props => (
+  <h1
+    style={{
+      fontWeight: 200,
+      fontSize: "3rem",
+      marginBottom: 30,
+      textTransform: "uppercase"
+    }}
+  >
+    {props.title}
+  </h1>
+);
 
-const TodoInput = props => {
-  return (
-    <div style={{ marginRight: "50px" }}>
-      <input
-        value={props.value}
-        className="todo-input"
-        type="text"
-        placeholder={props.placeholder}
-        onChange={props.onChange}
-        onKeyPress={props.onKeyPress}
-      />
-    </div>
-  );
-};
+const TodoInput = props => (
+  <div style={{ marginRight: "50px" }}>
+    <input
+      value={props.value}
+      className="todo-input"
+      type="text"
+      placeholder={props.placeholder}
+      onChange={props.onChange}
+      onKeyPress={props.onKeyPress}
+    />
+  </div>
+);
+
+const Todo = props => (
+  <div className="todo">
+    {props.content}
+  </div>
+);
+
+const TodoList = props => (
+  <div className="todo-list">
+    {props.todos.map(todo => (
+      <Todo content={todo.content} key={props.todos.indexOf(todo)} />
+    ))}
+  </div>
+);
 
 class App extends Component {
   constructor(props) {
@@ -47,7 +57,7 @@ class App extends Component {
     if (e.which === 13) {
       this.setState(prevState => {
         return {
-          todos: prevState.todos.concat([prevState.todoInput]),
+          todos: prevState.todos.concat([{content: prevState.todoInput, done: false}]),
           todoInput: ""
         };
       });
@@ -65,6 +75,7 @@ class App extends Component {
           placeholder="Enter Todo"
           onChange={this.onTodoInputChange}
         />
+        <TodoList todos={this.state.todos} />
       </div>
     );
   }
