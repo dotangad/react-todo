c52daa4573158d6148eda985999b7868664e32c9
Angad Singh <angadsingh567890@gmail.com>
2018-07-22 01:59:09 +0530

    Get functionality working

--- src/App.js
+++ src/App.js
@@ -1,30 +1,70 @@
 import React, { Component } from "react";
 
 const Header = props => {
-  return <h1 style={{
-    fontWeight: 200,
-    fontSize: '3rem',
-    marginBottom: 30
-  }}>{props.title}</h1>;
+  return (
+    <h1
+      style={{
+        fontWeight: 200,
+        fontSize: "3rem",
+        marginBottom: 30,
+        textTransform: "uppercase"
+      }}
+    >
+      {props.title}
+    </h1>
+  );
 };
 
 const TodoInput = props => {
   return (
-    <input
-    className
-    type="text"
-    value={props.value}
-    placeholder={props.placeholder}
-    />
+    <div style={{ marginRight: "50px" }}>
+      <input
+        value={props.value}
+        className="todo-input"
+        type="text"
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        onKeyPress={props.onKeyPress}
+      />
+    </div>
   );
-}
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoInput: "",
+      todos: []
+    };
+  }
+
+  onTodoInputChange = e => {
+    this.setState({ todoInput: e.target.value });
+  };
+
+  onTodoInputKeypress = e => {
+    if (e.which === 13) {
+      this.setState(prevState => {
+        return {
+          todos: prevState.todos.concat([prevState.todoInput]),
+          todoInput: ""
+        };
+      });
+      console.log(this.state);
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <Header title="Todo App" />
-        <TodoInput value="" placeholder="Enter Todo" />
+        <TodoInput
+          onKeyPress={this.onTodoInputKeypress}
+          value={this.state.todoInput}
+          placeholder="Enter Todo"
+          onChange={this.onTodoInputChange}
+        />
       </div>
     );
   }
